72b34deb124e630d87107fae565ef6465f6143c1
YaXioz <alfianzyh@gmail.com>
2025-06-16 23:29:16 +0700

    model n relasi

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 
     /**
      * The attributes that are mass assignable.
